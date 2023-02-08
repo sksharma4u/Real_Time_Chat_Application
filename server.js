901f2f0449569276e8d423b0d91e8d56b2550eae
sharmaji2612 <mahendrapal2612@gmail.com>
2023-02-09 01:01:24 +0530

    Server create

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const http = require("http").createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "*"
+    }
+});
+
+const user = {};
+
+http.listen(8000, function() {
+    io.on('connection', socket => { // This is an listen event it means server Listen all new updates , activities 
+        //  for example joining of new users.
+
+        socket.on('new-user-joined', name => { // This is an instance in whichh it hanles each connection , individually 
+            // According to their event
+            user[socket.id] = name;
+            socket.broadcast.emit('user-joined', name); // It brodacast the message to all users who already joined the  chat 
+        })
+
+        socket.on('send', message => {
+            socket.broadcast.emit('new-message', { message: message, name: user[socket.id] });
+        })
+
+        socket.on('disconnect', message => {
+            socket.broadcast.emit('left', user[socket.id]);
+            delete user[socket.id];
+        })
+
+    });
+
+})
